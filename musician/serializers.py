0ed931efe05646a92d98ebcb3c679919cf0efe9e
Danylo Skepskyi <kol230305@gmail.com>
2025-04-20 21:53:48 +0200

    fix flake8

--- musician/serializers.py
+++ musician/serializers.py
@@ -16,7 +16,6 @@ class MusicianSerializer(serializers.ModelSerializer):
             "is_adult"
         )
 
-
     def validate_age(self, value):
         if value < 14:
             raise serializers.ValidationError("age must be greater than 0")
