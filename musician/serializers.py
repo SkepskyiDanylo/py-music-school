80387e05c1b29d0547e887b666846dc5fb2886fc
Danylo Skepskyi <kol230305@gmail.com>
2025-04-20 21:48:16 +0200

    Solution

---
+++ musician/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Musician
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "instrument",
+            "age",
+            "date_of_applying",
+            "is_adult"
+        )
+
+    def validate_age(self, value):
+        if value < 14:
+            raise serializers.ValidationError("age must be greater than 0")
